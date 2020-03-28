a47421a82ba1bf03e0b35e8833afcdf1a694c60c
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2020-03-28 13:17:22 +0100

    add gorm to exp

--- exp/main.go
+++ exp/main.go
@@ -1,23 +1,77 @@
 package main
 
 import (
-	"html/template"
+	"bufio"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "123"
+	dbname   = "lenslocked_dev"
+)
+
+type User struct {
+	gorm.Model
+	Name   string
+	Email  string `gorm:"not null; unique_index"`
+	Orders []Order
+}
+
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
+}
+
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
+	db.LogMode(true)
+	db.AutoMigrate(&User{}, &Order{})
 
-	data := struct {
-		Name string
-		Age  int32
-	}{"obi-wan", 43}
+	var user User
+	db.Preload("Orders").First(&user)
+	if db.Error != nil {
+		panic(db.Error)
+	}
+	fmt.Println("Email:", user.Email)
+	fmt.Println("Number of orders:", len(user.Orders))
+	fmt.Println("Orders:", user.Orders)
+}
 
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
+func getInfo() (name, email string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+	email = strings.TrimSpace(email)
+	return name, email
+}
+
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
 	}
 }
