6fe2806f789e1346393c0056627931994e357055
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2020-06-02 18:33:48 +0200

    add persisting and prefilling user data on signup

--- controllers/users.go
+++ controllers/users.go
@@ -31,13 +31,16 @@ type Users struct {
 
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, r, nil)
+	var form SignupForm
+	parseURLParams(r, &form)
+	u.NewView.Render(w, r, form)
 }
 
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	var form SignupForm
+	vd.Yield = &form
 	if err := parseForm(r, &form); err != nil {
 		log.Println(err)
 		vd.SetAlert(err)
